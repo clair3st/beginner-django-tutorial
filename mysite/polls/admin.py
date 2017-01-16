03c054ec336512b4f684b8fbc03adfd9a1940b6a
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-15 18:07:16 -0800

    finished part 2

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+"""Model registed here."""
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Question
+
+admin.site.register(Question)
