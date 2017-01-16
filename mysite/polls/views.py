78d7768f1358b36ade8356840b27b6b13128276a
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-15 18:25:27 -0800

    completed part 3.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,51 @@
 """Django views are created here."""
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.http import Http404
 
 from django.http import HttpResponse
 
 
+from .models import Question
+
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
 def index(request):
-    """Most basic of views."""
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 """When Django finds a regular expression match, Django calls the specified
 view function, with an HttpRequest object as the first argument and any
 “captured” values from the regular expression as other arguments. If the
 regex uses simple captures, values are passed as positional arguments;
 if it uses named captures, values are passed as keyword arguments."""
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+    def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
